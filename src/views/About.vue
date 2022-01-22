d03bf46f83f6115f33f40f37ee9c291f3243e686
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-01-22 13:49:53 +0800

    修正日期

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,13 @@
   </div>
   <el-date-picker
     type="date"
+    v-model="dateValue"
     placeholder="Pick a date"
     style="width: 100%"
   ></el-date-picker>
 </template>
+
+<script setup>
+import { ref } from "vue";
+const dateValue = ref("");
+</script>
