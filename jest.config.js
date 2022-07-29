6db29cf854443f978d3bb8916050e62ef8dcae3e
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-07-29 10:25:46 +0800

    回復至原本jest

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,13 @@ module.exports = {
   collectCoverage: true,
   preset: "@vue/cli-plugin-unit-jest",
   setupFiles: ["./tests/unit/jest.init.js"],
-  reporters: [
-    "default",
-    [
-      "jest-html-reporters",
-      {
-        publicPath: "report/jest-html-reporters",
-      },
-    ],
-  ],
+  // reporters: [
+  //   "default",
+  //   [
+  //     "jest-html-reporters",
+  //     {
+  //       publicPath: "report/jest-html-reporters",
+  //     },
+  //   ],
+  // ],
 };
